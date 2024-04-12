d18a85ca7bb98891d356e7a7c42246117e0a3de2
Heiko Bornholdt <bornholdt@informatik.uni-hamburg.de>
2024-04-12 16:09:18 +0200

    Atom removed

--- vm-ubuntu-20.04/root-release-bootstrap.sh
+++ vm-ubuntu-20.04/root-release-bootstrap.sh
@@ -5,12 +5,6 @@ set -xe
 
 export DEBIAN_FRONTEND=noninteractive
 
-# Atom install steps came from this page on 2020-May-11:
-# https://flight-manual.atom.io/getting-started/sections/installing-atom/#platform-linux
-
-echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" | sudo tee /etc/apt/sources.list.d/atom.list
-wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | apt-key add -
-
 # Add repository with P4 packages
 # https://build.opensuse.org/project/show/home:p4lang
 
@@ -21,7 +15,6 @@ apt-get update -qq
 
 apt-get -qq -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
 apt-get install -qq -y --no-install-recommends --fix-missing\
-  atom \
   ca-certificates \
   curl \
   emacs \
